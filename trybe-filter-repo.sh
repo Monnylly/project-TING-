fe5c60f76dd8bc708ca98b52e83d0354c7efcd32
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-07-11 19:14:12 -0300

    Initial commit

---
+++ trybe-filter-repo.sh
@@ -0,0 +1,21 @@
+### GIT FILTER-REPO ###
+
+## NÃO EXECUTE ESSE SCRIPT DIRETAMENTE
+## Esse script foi feito para uso do
+## script 'publisher.sh' fornecido 
+## pela Trybe. 
+
+[[ $# == 1 ]] && \
+[[ $1 == "trybe-security-parameter" ]] && \
+git filter-repo \
+    --path .trybe \
+    --path .github \
+    --path .vscode \
+    --path trybe.yml \
+    --path trybe-filter-repo.sh \
+    --path tests/test_file_mangement.py \
+    --path tests/test_file_process.py \
+    --path tests/test_queue.py \
+    --path tests/test_word_search.py \
+    --path README.md \
+    --invert-paths --force
