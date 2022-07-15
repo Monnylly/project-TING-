71b4fb67e1516b8d899d02d6b71cc95ced755556
Monnylly <monnylly@gmail.com>
2022-07-15 18:29:00 -0300

    feat: increment function txt importer requirement two

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,4 +1,13 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
 
-    # comit initial
+
+def txt_importer(path_file: str):
+    try:
+        if not path_file.endswith(".txt"):
+            sys.stderr.write("Formato inválido\n")
+        with open(path_file) as file:
+            return file.read().splitlines()
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
