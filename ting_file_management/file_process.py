ece2c0e97199a04261a135f89906f3bb2369069b
Monnylly <monnylly@gmail.com>
2022-07-18 11:11:58 -0300

    feat: implement requirements four and five

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+import sys
+from ting_file_management.queue import Queue
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def process(path_file, instance: Queue):
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return
+
+    file = txt_importer(path_file)
+    process = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    instance.enqueue(process)
+    return sys.stdout.write(str(process))
 
 
-def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue) -> None:
+    if not len(instance):
+        return sys.stdout.write("Não há elementos\n")
+    else:
+        result_remove = instance
+        ["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {result_remove} removido com sucesso\n")
+
+
+def file_metadata(instance: Queue, position: int):
+    try:
+        result_file = instance.search(position)
+        sys.stdout.write(str(result_file))
+    except IndexError:
+        return sys.stderr.write("Posição inválida\n")
